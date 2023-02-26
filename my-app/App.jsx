72f6531d167cd0c38c8367e31358225cfd003d92
horii203 <89522294+horii203@users.noreply.github.com>
2023-02-26 15:44:17 +0900

    メモリスト画面作成

---
+++ my-app/App.jsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.appbar}>
+        <View style={styles.appbarInner}>
+          <Text style={styles.appbarTitle}>Memo App</Text>
+          <Text style={styles.appbarRight}>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023年1月1日 10:00</Text>
+          </View>
+
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023年1月1日 10:00</Text>
+          </View>
+
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023年1月1日 10:00</Text>
+          </View>
+
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+
+      </View>
+
+      <View style={styles.circleButtom}>
+        <Text style={styles.circleButtomLabel}>+</Text>
+      </View>
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f4f8',
+  },
+
+  appbar: {
+    width: '100%',
+    height: 104,
+    backgroundColor: '#467fd3',
+    justifyContent: 'flex-end',
+
+  },
+
+  appbarInner: {
+    alignItems: 'center'
+  },
+  
+  appbarRight: {
+    position: 'absolute',
+    right: 19,
+    bottom: 16,
+    color: 'rgba(255, 255, 255, 0.8)'
+  },
+
+  appbarTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+
+  memoListItem: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.15)'
+  },
+
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+
+  memoListItemDate: {
+    fontSize: 16,
+    color: '#848484'
+  },
+
+  circleButtom: {
+    backgroundColor: '#467fd3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position:'absolute',
+    right: 40,
+    bottom: 40,
+    // 影の設定
+    shadowColor: '#000',
+    shadowOffset: {width: 8, height: 8},
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,  // android用
+  },
+
+  circleButtomLabel: {
+    color: '#fff',
+    fontSize: 40,
+    lineHeight: 40
+  }
+});
