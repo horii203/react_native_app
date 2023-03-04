c09b5b33c921699eabd6a5d30718df8830b3f765
horii203 <89522294+horii203@users.noreply.github.com>
2023-03-04 18:59:54 +0900

    ログイン画面作成

--- my-app/App.jsx
+++ my-app/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 // import MemoListScreen from './src/screens/MemoListScreen';
-import MemoCreatScreen from './src/screens/MemoCreatScreen';
+import LogInScreen from './src/screens/LogInScreen';
 
 export default function App() {
   return (
-    <MemoCreatScreen/>
+    <LogInScreen/>
   );
 }
---
+++ my-app/src/screens/LogInScreen.jsx
@@ -0,0 +1,87 @@
+// ログイン画面
+import React from 'react';
+import { StyleSheet, View, TextInput, Text } from 'react-native';
+import AppBar from '../components/AppBar';
+
+export default function LogInScreen() {
+  return (
+    <View style={styles.container}>
+        <AppBar/>
+
+        <View style={styles.inner}>
+            <Text style={styles.title}>Log In</Text>
+            <TextInput style={styles.input} value='Email Adress'/>
+            <TextInput style={styles.input} value='Password'/>
+
+            <View style={styles.buttonContainer}>
+                <Text style={styles.buttonLabel}>Submit</Text>
+            </View>
+
+            <View style={styles.footer}>
+                <Text style={styles.footerText}>Not registered?</Text>
+                <Text style={styles.footerLink}>Sign up here</Text>
+            </View>
+        </View>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f4f8'
+  },
+
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24
+  },
+
+  input: {
+    fontSize: 16,
+    height: 48,
+    borderColor: '#dddddd',
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal:8,
+    marginBottom: 16
+  },
+
+  buttonContainer: {
+    backgroundColor: '#467fd3',
+    borderRadius: 4,
+    alignSelf: 'flex-start',
+    marginBottom: 24
+  },
+
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    color: '#fff'
+  },
+
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8
+  },
+
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467fd3',
+  },
+
+  footer: {
+    flexDirection: 'row'
+  }
+})
